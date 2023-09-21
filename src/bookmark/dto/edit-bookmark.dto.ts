5f3df554298848b087976a215eaaaedecc4189f5
minhng-178 <anhminh1782002@gmail.com>
2023-09-21 19:32:43 +0700

    learn-nestjs

---
+++ src/bookmark/dto/edit-bookmark.dto.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import {
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class EditBookmarkDto {
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  link?: string;
+}
---
+++ src/bookmark/dto/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export * from './create-bookmark.dto';
+export * from './edit-bookmark.dto';
